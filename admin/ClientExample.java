a248ad6557962899b218c3d0e95fc5d0914016ea
Binh Ly <binh.ly@hibalo.com>
2014-03-07 17:50:33 -0500

    client connect disconnect

--- admin/ClientExample.java
+++ admin/ClientExample.java
@@ -1,5 +1,6 @@
 package admin;
 
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
@@ -16,5 +17,18 @@ public class ClientExample {
         Client result = new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
         
         return result;
+    }
+    
+    public static void connectDisconnect() {
+        //connect
+        Client client = newTransportClient();
+        
+        ClusterHealthResponse response = client.admin().cluster().prepareHealth().execute().actionGet();
+        System.out.println(
+            "Cluster Name: " + response.getClusterName() + "\n" +
+            "Cluster Health: " + response.getStatus());
+        
+        //disconnect
+        client.close();
     }
 }
