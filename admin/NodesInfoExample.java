43ce584fdcb7eca54f5a1ec9bc163e9bc38d6d60
Binh Ly <binh.ly@hibalo.com>
2014-04-25 15:46:41 -0400

    indices info

--- admin/NodesInfoExample.java
+++ admin/NodesInfoExample.java
@@ -1,20 +1,33 @@
 package admin;
 
 import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
+import org.elasticsearch.action.admin.indices.stats.IndexStats;
+import org.elasticsearch.action.admin.indices.stats.IndicesStatsRequest;
+import org.elasticsearch.action.admin.indices.stats.IndicesStatsResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
+import java.util.Map;
 
 public class NodesInfoExample {
     public static void Nodes(Client client){
         NodesInfoRequest nodesInfoRequest = new NodesInfoRequest();
         nodesInfoRequest.clear();
-        //client .admin().cluster().nodesInfo(nodesInfoRequest)
         NodesInfoResponse response = client.admin().cluster().nodesInfo(nodesInfoRequest).actionGet();
         System.out.println("Cluster Nodes: " + response.getNodes().length);
         for (NodeInfo node: response.getNodes()) {
             System.out.println("Node: " + node.getNode().name());
+        }
+    }
+
+    public static void Indices(Client client) {
+        IndicesStatsRequest indicesStatsRequest = new IndicesStatsRequest();
+        indicesStatsRequest.clear();
+        IndicesStatsResponse response = client.admin().indices().stats(indicesStatsRequest).actionGet();
+        System.out.println("Cluster Indexes: " + response.getIndices().size());
+        for (Map.Entry<String, IndexStats> i: response.getIndices().entrySet()) {
+            System.out.println("Index: " + i.getKey());
         }
     }
 }
